8a8595bcc4f347c093d69944d2ba9633901d14e7
Fan Liu <liufan321@gmail.com>
2015-01-01 08:57:38 +0800

    初始化项目

---
+++ DGCryptoTools/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DGCryptoTools
+//
+//  Created by 刘凡 on 15/1/1.
+//  Copyright (c) 2015年 joyios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
